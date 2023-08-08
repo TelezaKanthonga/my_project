539dfe18eb1efc8b392ce88b84950916aacdc4ee
Teleza Kanthonga <telezakanthonga@gmail.com>
2023-08-08 14:05:04 +0200

    Machine Learning on R

--- machine_learning.R
+++ machine_learning.R
@@ -34,12 +34,100 @@ dat_rescale <- dat_drop %>%
   mutate_if(is.numeric, list(~as.numeric(scale(.))))
 head(dat_rescale)
 
+#Check factors,define new levels
+#The goal is to figure out which factors influence this model/system
+#select categorical column
+factor <- data.frame(select_if(dat_rescale,is.factor))
+ ncol(factor)
+
+#plot graphs for each column
+ library(ggplot2)
+graph <- lapply(names(factor),
+                function(x)
+                  ggplot(factor,aes(get(x))) +
+                geom_bar() +
+                  theme(axis.text.x = element_text(angle =90)))
+
+graph 
+
+#plot gender income
+ggplot(dat_rescale,
+       aes(x = Gender, fill = Loan_status)) +
+  geom_bar(position = "fill") +
+  theme_classic()
+
+#plot education loan status
+ggplot(dat_rescale,
+       aes(x = Education, fill = Loan_status)) +
+  geom_bar(position = "fill") +
+  theme_classic()
+theme(axis.text.x = element_text(angle =90))
+
+#plot property area loan status
+ggplot(dat_rescale,
+       aes(x = Property_area, fill = Loan_status)) +
+  geom_bar(position = "fill") +
+  theme_classic()
+theme(axis.text.x = element_text(angle =90))
+
+#Check if applicant income is related to loan
+
+ggplot(dat_rescale, aes(x = ApplicantIncome, y = Loan_amount)) +
+  geom_point(aes(color = Loan_status),
+             size = 0.5) +
+  stat_smooth(method = "lm",
+formula = y~poly(x, 2),
+se =TRUE,
+aes(color = Loan_status)) +
+  theme_classic()
 
 
+#visualize betwween variables
+install.packages("GGally")
+library(GGally)
+#convert data to numeric
+corr <- data.frame(lapply(dat_rescale, as.integer))
+#plot the graph
+ggcorr(corr,
+       method = c("pairwise", "spearman"),
+       nbreaks = 6,
+       hjust =0.8,
+       label =TRUE,
+       label_size =3,
+       color ="grey50")
 
 
+#train /test set
+install.packages("caret")
+library(caret)
+set.seed(1234)
+
+trainIndex <- createDataPartition(dat$Loan_status, p =0.7,
+                                  list =FALSE, times =1)
+trainData <- dat[trainIndex,]
+testData <- dat[-trainIndex,]
+
+print(dim(trainData)); print(dim(testData))
+
+#Build modelm
+model_glm = glm(Loan_status~., family="binomial",
+                data = trainData)
+                                  
+summary(model_glm)
+
+#Baseline Accurancy
+prop.table(table(trainData$Loan_status))
 
 
+#Assess the performance of the model
+#prediction on the training set
+predictTrain <- predict(model_glm, trainData, type = "response")
 
 
-
+#Confusion matrix on training data
+train_mat <- table(trainData$Loan_status, predictTrain >= 0.5)
+                                  
+train_mat                              
+                                  
+                                  
+              
